7c2871ab7e45b32199db0f78b55e7f4f9e3a9cd6
Alex Thomson <LXGaming@users.noreply.github.com>
2021-07-23 00:53:41 +1200

    Add HomeController

---
+++ LXGaming.Ticket.Server/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LXGaming.Ticket.Server.Controllers {
+
+    [ApiController]
+    [Route("")]
+    public class HomeController : ControllerBase {
+
+        [HttpGet]
+        public IActionResult Get() {
+            return Ok(new {
+                Application = "Ticket API"
+            });
+        }
+    }
+}
