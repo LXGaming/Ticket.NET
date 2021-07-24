9714ccddbde9a4c733d1d628815a5a11e3252a60
Alex Thomson <LXGaming@users.noreply.github.com>
2021-07-24 19:16:45 +1200

    Push Toolbox class

---
+++ LXGaming.Ticket.Server/Util/Toolbox.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace LXGaming.Ticket.Server.Util {
+
+    public static class Toolbox {
+
+        public static bool ParseIdentifier(string identifier, out string key, out string value) {
+            var strings = identifier?.Split(':', 2, StringSplitOptions.RemoveEmptyEntries);
+            if (strings?.Length == 2) {
+                key = strings[0];
+                value = strings[1];
+                return true;
+            }
+
+            key = null;
+            value = null;
+            return false;
+        }
+    }
+}
