40e0d76f7a18b4eefea7f49890c81534883ba2a7
Alex Thomson <LXGaming@users.noreply.github.com>
2021-07-26 20:43:18 +1200

    Push Extensions class

---
+++ LXGaming.Ticket.Server/Util/Extensions.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq;
+using LXGaming.Ticket.Server.Models;
+
+namespace LXGaming.Ticket.Server.Util {
+
+    public static class Extensions {
+
+        public static string GetUserNameValue(this Issue issue, string defaultValue = null) {
+            return GetUserNameValue(issue.User, issue.ProjectId);
+        }
+
+        public static UserName GetUserName(this Issue issue) {
+            return GetUserName(issue.User, issue.ProjectId);
+        }
+
+        public static string GetUserNameValue(this User user, string projectId, string defaultValue = null) {
+            return GetUserName(user, projectId)?.Value ?? defaultValue;
+        }
+
+        public static UserName GetUserName(this User user, string projectId) {
+            return user.Names?.SingleOrDefault(model => string.Equals(model.ProjectId, projectId));
+        }
+    }
+}
